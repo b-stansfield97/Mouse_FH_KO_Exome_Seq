31da654378a96bd89bfdaee61abf405fe08f7868
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-24 11:48:32 -0700

    Add files via upload

---
+++ Mouse_Exome_Seq_BWA_Allignment.sh
@@ -0,0 +1,6 @@
+bwa mem -P /data/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa MMCID-P_R1.fastq.gz MMCID-P_R2.fastq.gz > MMCID-P.sam &
+bwa mem -P /data/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa MMCID-26B_R1.fastq.gz MMCID-26B_R2.fastq.gz > MMCID-26B.sam &
+bwa mem -P /data/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa MMCID-30B_R1.fastq.gz MMCID-30B_R2.fastq.gz > MMCID-30B.sam &
+bwa mem -P /data/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa TMCK1-P_R1.fastq.gz TMCK1-P_R2.fastq.gz > TMCK1-P.sam &
+bwa mem -P /data/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa TMCK1-14B_R1.fastq.gz TMCK1-14B_R2.fastq.gz > TMCK1-14B.sam &
+bwa mem -P /data/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa TMCK1-23B_R1.fastq.gz TMCK1-23B_R2.fastq.gz > TMCK1-24B.sam & 
