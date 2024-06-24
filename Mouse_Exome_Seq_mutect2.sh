b996edbd62c6e21befcfa85ffe069021d81fca8f
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-24 16:04:39 -0700

    Add files via upload

---
+++ Mouse_Exome_Seq_mutect2.sh
@@ -0,0 +1,15 @@
+gatk Mutect2 \
+     -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+     -I MMCID-P-sorted.bam \
+     -I MMCID-26B-sorted.bam \
+     -I MMCID-30B-sorted.bam\
+     -normal MMCID-P \
+     -O MMCID.vcf.gz &
+
+gatk Mutect2 \
+    -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+    -I TMCK1-P-sorted.bam \
+    -I TMCK1-14B-sorted.bam \
+    -I TMCK1-24B-sorted.bam \
+    -normal TMCK1-P \
+    -O TMCK1.vcf.gz &
