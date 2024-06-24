ce6792eff0c1e77d8e91fc8f2bfdf76ab475c371
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-24 16:04:04 -0700

    Add files via upload

---
+++ Mouse_Exome_Seq_Sort_BAM_files.sh
@@ -0,0 +1,6 @@
+samtools sort -o MMCID-P-sorted.bam -@ 10 fnd_MMCID-P.bam &
+samtools sort -o MMCID-26B-sorted.bam -@ 10 fnd_MMCID-26B.bam &
+samtools sort -o MMCID-30B-sorted.bam -@ 10 fnd_MMCID-30B.bam &
+samtools sort -o TMCK1-P-sorted.bam -@ 10 fnd_TMCK1-P.bam &
+samtools sort -o TMCK1-14B-sorted.bam -@ 10 fnd_TMCK1-14B.bam &
+samtools sort -o TMCK1-24B-sorted.bam -@ 10 fnd_TMCK1-24B.bam &
