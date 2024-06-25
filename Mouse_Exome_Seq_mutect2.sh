c1e0df86bf4787917a6d7cc6d7ac13b2f5e08d7d
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-25 15:41:46 -0700

    Update Mouse_Exome_Seq_mutect2.sh

--- Mouse_Exome_Seq_mutect2.sh
+++ Mouse_Exome_Seq_mutect2.sh
@@ -2,14 +2,26 @@ gatk Mutect2 \
      -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
      -I MMCID-P-sorted.bam \
      -I MMCID-26B-sorted.bam \
+     -normal MMCIDP \
+     -O MMCID-26B.vcf.gz &
+
+gatk Mutect2 \
+     -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+     -I MMCID-P-sorted.bam \
      -I MMCID-30B-sorted.bam\
      -normal MMCIDP \
-     -O MMCID.vcf.gz &
+     -O MMCID-30B.vcf.gz &
 
 gatk Mutect2 \
     -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
     -I TMCK1-P-sorted.bam \
     -I TMCK1-14B-sorted.bam \
+    -normal TMCK1P \
+    -O TMCK1-14B.vcf.gz &
+
+    gatk Mutect2 \
+    -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+    -I TMCK1-P-sorted.bam \
     -I TMCK1-24B-sorted.bam \
     -normal TMCK1P \
-    -O TMCK1.vcf.gz &
+    -O TMCK1-24B.vcf.gz &
