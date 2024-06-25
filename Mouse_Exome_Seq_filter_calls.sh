a169d08c52cc99715f0605257e4c58c6599dcafe
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-25 15:42:41 -0700

    Create Mouse_Exome_Seq_filter_calls.sh

---
+++ Mouse_Exome_Seq_filter_calls.sh
@@ -0,0 +1,4 @@
+gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V MMCID-26B.vcf.gz -O MMCID-26B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V MMCID-30B.vcf.gz -O MMCID-30B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V TMCK1-14B.vcf.gz -O TMCK1-14B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V TMCK1-24B.vcf.gz -O TMCK1-24B_filtered.vcf.gz &
