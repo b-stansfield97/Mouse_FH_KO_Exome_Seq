f718c2e010627c729591666abd1cfe0d694319db
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-27 09:46:20 -0700

    Update Mouse_Exome_Seq_Fix_Read_groups.sh

--- Mouse_Exome_Seq_Fix_Read_groups.sh
+++ Mouse_Exome_Seq_Fix_Read_groups.sh
@@ -3,4 +3,4 @@ java -jar picard.jar AddOrReplaceReadGroups I=nd_MMCID-26B.bam O=fnd_MMCID-26B.b
 java -jar picard.jar AddOrReplaceReadGroups I=nd_MMCID-30B.bam O=fnd_MMCID-30B.bam RGID=4 RGLB=lib3 RGPL=ILLUMINA RGPU=unit3 RGSM=MMCID30B &
 java -jar picard.jar AddOrReplaceReadGroups I=nd_TMCK1-P.bam O=fnd_TMCK1-P.bam RGID=3 RGLB=lib4 RGPL=ILLUMINA RGPU=unit4 RGSM=TMCK1P &
 java -jar picard.jar AddOrReplaceReadGroups I=nd_TMCK1-14B.bam O=fnd_TMCK1-14B.bam RGID=2 RGLB=lib5 RGPL=ILLUMINA RGPU=unit5 RGSM=TMCK114B &
-java -jar picard.jar AddOrReplaceReadGroups I=nd_TMCK1-24B.bam O=fnd_TMCK1-24B.bam RGID=1 RGLB=lib6 RGPL=ILLUMINA RGPU=unit6 RGSM=TMCK124B &
+java -jar picard.jar AddOrReplaceReadGroups I=nd_TMCK1-23B.bam O=fnd_TMCK1-23B.bam RGID=1 RGLB=lib6 RGPL=ILLUMINA RGPU=unit6 RGSM=TMCK123B &
