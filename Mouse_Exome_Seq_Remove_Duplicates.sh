570bfc5232384750a9b9ac3ce3be276caef412c3
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-27 09:45:55 -0700

    Update Mouse_Exome_Seq_Remove_Duplicates.sh

--- Mouse_Exome_Seq_Remove_Duplicates.sh
+++ Mouse_Exome_Seq_Remove_Duplicates.sh
@@ -3,4 +3,4 @@ java -jar picard.jar MarkDuplicates -REMOVE_DUPLICATES TRUE -I sorted_MMCID-26B.
 java -jar picard.jar MarkDuplicates -REMOVE_DUPLICATES TRUE -I sorted_MMCID-30B.sam -O nd_MMCID-30B.bam -M MMCID-30B_metric.txt
 java -jar picard.jar MarkDuplicates -REMOVE_DUPLICATES TRUE -I sorted_TMCK1-P.sam -O nd_TMCK1-P.bam -M TMCK1-P_metric.txt
 java -jar picard.jar MarkDuplicates -REMOVE_DUPLICATES TRUE -I sorted_TMCK1-14B.sam -O nd_TMCK1-14B.bam -M TMCK1-14B_metric.txt
-java -jar picard.jar MarkDuplicates -REMOVE_DUPLICATES TRUE -I sorted_TMCK1-24B.sam -O nd_TMCK1-24B.bam -M TMCK1-24B_metric.txt
+java -jar picard.jar MarkDuplicates -REMOVE_DUPLICATES TRUE -I sorted_TMCK1-23B.sam -O nd_TMCK1-23B.bam -M TMCK1-23B_metric.txt
