b93687deb8a5fd0435cdd4c2071ba023fbf6350f
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-27 09:46:44 -0700

    Update Mouse_Exome_Seq_Sort_BAM_files.sh

--- Mouse_Exome_Seq_Sort_BAM_files.sh
+++ Mouse_Exome_Seq_Sort_BAM_files.sh
@@ -3,4 +3,4 @@ samtools sort -o MMCID-26B-sorted.bam -@ 10 fnd_MMCID-26B.bam &
 samtools sort -o MMCID-30B-sorted.bam -@ 10 fnd_MMCID-30B.bam &
 samtools sort -o TMCK1-P-sorted.bam -@ 10 fnd_TMCK1-P.bam &
 samtools sort -o TMCK1-14B-sorted.bam -@ 10 fnd_TMCK1-14B.bam &
-samtools sort -o TMCK1-24B-sorted.bam -@ 10 fnd_TMCK1-24B.bam &
+samtools sort -o TMCK1-23B-sorted.bam -@ 10 fnd_TMCK1-23B.bam &
