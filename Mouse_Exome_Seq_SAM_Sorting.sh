5aba791d9237a7ed1f2bd4ca48015240436d2c0b
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-28 09:49:49 -0700

    Update and rename Mouse_Exome_Seq_Sorting.sh to Mouse_Exome_Seq_SAM_Sorting.sh

---
+++ Mouse_Exome_Seq_SAM_Sorting.sh
@@ -0,0 +1,6 @@
+samtools sort -@ 10 MMCID-P.sam  &
+samtools sort -@ 10 MMCID-26B.sam &
+samtools sort -@ 10 MMCID-30B.sam &
+samtools sort -@ 10 TMCK1-P.sam &
+samtools sort -@ 10 TMCK1-14B.sam &
+samtools sort -@ 10 TMCK1-23B.sam &
--- Mouse_Exome_Seq_Sorting.sh
+++
@@ -1,6 +0,0 @@
-samtools sort MMCID-P.sam  &
-samtools sort MMCID-26B.sam &
-samtools sort MMCID-30B.sam &
-samtools sort TMCK1-P.sam &
-samtools sort TMCK1-14B.sam &
-samtools sort TMCK1-23B.sam &
