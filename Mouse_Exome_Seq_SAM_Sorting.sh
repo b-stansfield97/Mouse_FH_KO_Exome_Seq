54ea958ca3e84bc4f7a8ef65dcfe9fc451c7a9fe
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-28 10:26:13 -0700

    Update Mouse_Exome_Seq_SAM_Sorting.sh

--- Mouse_Exome_Seq_SAM_Sorting.sh
+++ Mouse_Exome_Seq_SAM_Sorting.sh
@@ -1,6 +1,6 @@
-samtools sort -@ 10 MMCID-P.sam  &
-samtools sort -@ 10 MMCID-26B.sam &
-samtools sort -@ 10 MMCID-30B.sam &
-samtools sort -@ 10 TMCK1-P.sam &
-samtools sort -@ 10 TMCK1-14B.sam &
-samtools sort -@ 10 TMCK1-23B.sam &
+samtools sort -@ 10 MMCID-P.sam > sorted_MMCID-P.sam &
+samtools sort -@ 10 MMCID-26B.sam > sorted_MMCID-26B.sam &
+samtools sort -@ 10 MMCID-30B.sam > sorted_MMCID-30B.sam &
+samtools sort -@ 10 TMCK1-P.sam > sorted_TMCK1-P.sam &
+samtools sort -@ 10 TMCK1-14B.sam > sorted_TMCK1-14B.sam &
+samtools sort -@ 10 TMCK1-23B.sam > sorted_TMCK1-23B.sam &
