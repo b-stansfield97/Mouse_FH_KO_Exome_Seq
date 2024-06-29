8e9a57b341c5c6e140c04f7ebc4943acc10257c4
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-29 16:12:13 -0700

    Update Mouse_Exome_Seq_mutect2.sh

--- Mouse_Exome_Seq_mutect2.sh
+++ Mouse_Exome_Seq_mutect2.sh
@@ -1,27 +1,27 @@
 gatk Mutect2 \
-     -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+     -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa \
      -I MMCID-P-sorted.bam \
      -I MMCID-26B-sorted.bam \
      -normal MMCIDP \
      -O MMCID-26B.vcf.gz &
 
 gatk Mutect2 \
-     -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+     -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa \
      -I MMCID-P-sorted.bam \
      -I MMCID-30B-sorted.bam\
      -normal MMCIDP \
      -O MMCID-30B.vcf.gz &
 
 gatk Mutect2 \
-    -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+    -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa \
     -I TMCK1-P-sorted.bam \
     -I TMCK1-14B-sorted.bam \
     -normal TMCK1P \
     -O TMCK1-14B.vcf.gz &
 
     gatk Mutect2 \
-    -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+    -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa \
     -I TMCK1-P-sorted.bam \
-    -I TMCK1-24B-sorted.bam \
+    -I TMCK1-23B-sorted.bam \
     -normal TMCK1P \
-    -O TMCK1-24B.vcf.gz &
+    -O TMCK1-23B.vcf.gz &
