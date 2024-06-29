fb1eb11bd4457c22318cc856f997a83d9d5b85ba
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-29 16:13:38 -0700

    Update Mouse_Exome_Seq_VEP.sh

--- Mouse_Exome_Seq_VEP.sh
+++ Mouse_Exome_Seq_VEP.sh
@@ -1,35 +1,62 @@
-perl vep \
-    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/MMCID-26B_filtered_clean.vcf \
-    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/MMCID-26B_annotated.vcf \
+perl /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep/vep \
+    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/MMCID-26B_filtered_clean.vcf \
+    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/MMCID-26B_annotated.vcf \
+    --cache \
+    --vcf \
+    --dir_cache /home/bstansfield/.vep \
     --species mus_musculus \
     --assembly GRCm39 \
+    --refseq \
+    --force_overwrite \
     --offline \
-    --cache /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
-    --dir_cache /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep &
+    --fasta /home/bstansfield/.vep \
+    --everything \
+    --variant_class \
+    --show_ref_allele &
 
-perl vep \
-    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/MMCID-30B_filtered_clean.vcf \
-    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/MMCID-30B_annotated.vcf \
+perl /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep/vep \
+    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/MMCID-30B_filtered_clean.vcf \
+    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/MMCID-30B_annotated.vcf \
+    --cache \
+    --vcf \
+    --dir_cache /home/bstansfield/.vep \
     --species mus_musculus \
     --assembly GRCm39 \
+    --refseq \
     --offline \
-    --cache /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
-    --dir_cache /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep &
+    --fasta /home/bstansfield/.vep \
+    --everything \
+    --variant_class \
+    --show_ref_allele &
 
-perl vep \
-    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/TMCK1-14B_filtered_clean.vcf  \
-    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/TMCK1-14B_annotated.vcf \
+perl /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep/vep \
+    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/TMCK1-14B_filtered_clean.vcf  \
+    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/TMCK1-14B_annotated.vcf \
+    --cache \
+    --vcf \
+    --dir_cache /home/bstansfield/.vep \
     --species mus_musculus \
     --assembly GRCm39 \
+    --refseq \
+    --force_overwrite  \
     --offline \
-    --cache /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
-    --dir_cache /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep &
+    --fasta /home/bstansfield/.vep \
+    --everything \
+    --variant_class \
+    --show_ref_allele &
 
-perl vep \
-    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/TMCK1-24B_filtered_clean.vcf \
-    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/TMCK1-24B_annotated.vcf \
+perl /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep/vep \
+    --input_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/TMCK1-24B_filtered_clean.vcf  \
+    --output_file /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/final_vcf/TMCK1-24B_annotated.vcf \
+    --cache \
+    --vcf \
+    --dir_cache /home/bstansfield/.vep \
     --species mus_musculus \
     --assembly GRCm39 \
+    --refseq \
+    --force_overwrite \
     --offline \
-    --cache /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
-    --dir_cache /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/ensembl-vep &
+    --fasta /home/bstansfield/.vep \
+    --everything \
+    --variant_class \
+    --show_ref_allele &
