0b63da7f79a4a3bf431f4eb39dc12faf7e8d6716
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-29 16:13:08 -0700

    Update Mouse_Exome_Seq_filter_calls.sh

--- Mouse_Exome_Seq_filter_calls.sh
+++ Mouse_Exome_Seq_filter_calls.sh
@@ -1,4 +1,4 @@
-gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V MMCID-26B.vcf.gz -O MMCID-26B_filtered.vcf.gz &
-gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V MMCID-30B.vcf.gz -O MMCID-30B_filtered.vcf.gz &
-gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V TMCK1-14B.vcf.gz -O TMCK1-14B_filtered.vcf.gz &
-gatk FilterMutectCalls -R /data/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -V TMCK1-24B.vcf.gz -O TMCK1-24B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa -V MMCID-26B.vcf.gz -O MMCID-26B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa -V MMCID-30B.vcf.gz -O MMCID-30B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa -V TMCK1-14B.vcf.gz -O TMCK1-14B_filtered.vcf.gz &
+gatk FilterMutectCalls -R /data/mouse_exome_seq/Samples/DefaultProject/FASTQ/mouse_reference/Mus_musculus.GRCm39.dna.primary_assembly.fa -V TMCK1-23B.vcf.gz -O TMCK1-23B_filtered.vcf.gz &
