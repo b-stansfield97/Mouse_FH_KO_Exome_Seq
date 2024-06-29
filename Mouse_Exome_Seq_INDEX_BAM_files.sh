d8590d4b343e58522d4f2632970de9b4ecb7852f
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-29 16:41:00 -0700

    Update Mouse_Exome_Seq_INDEX_BAM_files.sh

--- Mouse_Exome_Seq_INDEX_BAM_files.sh
+++ Mouse_Exome_Seq_INDEX_BAM_files.sh
@@ -3,4 +3,4 @@ samtools index -@ 10 MMCID-26B-sorted.bam &
 samtools index -@ 10 MMCID-30B-sorted.bam &
 samtools index -@ 10 TMCK1-P-sorted.bam &
 samtools index -@ 10 TMCK1-14B-sorted.bam &
-samtools index -@ 10 TMCK1-24B-sorted.bam &
+samtools index -@ 10 TMCK1-23B-sorted.bam &
