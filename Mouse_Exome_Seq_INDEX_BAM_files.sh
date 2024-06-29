fb79e38035ebd69e73c9a5cec0939e1632ccead0
Ben Stansfield <137545673+b-stansfield97@users.noreply.github.com>
2024-06-29 16:09:37 -0700

    Create Mouse_Exome_Seq_INDEX_BAM_files.sh

---
+++ Mouse_Exome_Seq_INDEX_BAM_files.sh
@@ -0,0 +1,6 @@
+samtools index -@ 10 MMCID-P-sorted.bam &
+samtools index -@ 10 MMCID-26B-sorted.bam &
+samtools index -@ 10 MMCID-30B-sorted.bam &
+samtools index -@ 10 TMCK1-P-sorted.bam &
+samtools index -@ 10 TMCK1-14B-sorted.bam &
+samtools index -@ 10 TMCK1-24B-sorted.bam &
